9032614193543b934e3da247be94190a615ed6d6
zjloop <zjloop2015@gmail.com>
2017-10-14 09:51:07 +1300

    This is the change

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extends:false}));
 
+var people = [
+    {
+    name:'Stephen',
+    age: 10
+    },
+    {
+        name:'Lin',
+        age:2
+    }
+]
 app.get('/', function(req, res) {
-            res.send('this is the other');
+            res.json(people);
         })
 app.listen(3000);
