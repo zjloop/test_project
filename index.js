6ded1ade5127a151cbb029c27e8471c321c1df91
zjloop <zjloop2015@gmail.com>
2017-10-14 09:57:08 +1300

    1

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extends:false}));
 
 var people = [
     {
