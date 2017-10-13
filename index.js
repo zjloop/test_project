ca1bbdab97bd47042efdca02ccd3fd2f9e5ffb05
zjloop <zjloop2015@gmail.com>
2017-10-14 09:44:30 +1300

    otherchange

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-fdsfasd
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends:false}));
+
+app.get('/', function(req, res) {
+            res.send('this is the other');
+        })
+app.listen(3000);
