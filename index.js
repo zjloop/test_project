3841010a1c856ec3ad1cfdc6bf69dd1f95c4f70b
zjloop <zjloop2015@gmail.com>
2017-10-14 15:30:29 +1300

    2

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-var people = [
+var users = [
     {
-    name:'Stephen',
-    age: 10
+        id: 1,
+        firstName: "Jone",
+        lastName: "Z"
     },
     {
-        name:'Lin',
-        age:2
+        id: 2,
+        firstName: "Jill",
+        lastName: "Z"
+    },
+    {
+        id: 3,
+        firstName: "Susan",
+        lastName: "Z"
     }
 ]
-app.get('/', function(req, res) {
-            res.json(people);
-        })
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'name',
+        users: users
+    });
+})
+
+app.post('/users/add', function(req, res) {
+
+    var result = {
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+    }
+    console.log(result);
+    res.redirect('/complete');
+    
+})
 app.listen(3000);
